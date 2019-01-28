95e0db77084724b9220713baa46213a841efbbc9
Nate Vogel <nvogel@clsphila.org>
2019-01-28 11:38:26 -0500

    credentials store the name of the report as a section in the .ini file. Also reports.R can download a report with credentials from credentials.R. I implemented a method for tranforming the xml to a dataframe, but its wayyyy to slow. It used all the mem and cpu of my machine, and killed the R session. This, when using report with around 20k rows and 10 or so columns.

--- R/credentials.R
+++ R/credentials.R
@@ -4,31 +4,38 @@
 # Get gpg-encrypted credentials for accessing the LegalServer Report api.
 get.credentials <- function(credentials.path = "./credentials.gpg") {
   tmpfile <- tempfile()
-  credentials.path %>%
-    gpg::gpg_decrypt(.) %>%
-    writeLines(con = tmpfile)
-
-  configr::read.config(file = tmpfile) %>%
-    return
+  writeLines(
+    gpg::gpg_decrypt(credentials.path),
+    con = tmpfile
+  )
+  on.exit(unlink(tmpfile))
+  return(
+    configr::read.config(file = tmpfile))
 }
 
 # Save credentials
 #
 # Ask a user for credentials to download reports from LegalServer, and
 # save those credentials to a file encrypted with gpg.
-save.credentials <- function(path.for.secrets = "./credentials.gpg", receiver) {
+save.credentials <- function(receiver, path.for.secrets = "./credentials.gpg", return.creds = FALSE) {
   writeLines("Please enter the legalserver api credentials");
   tmpfile <- tempfile()
-  configr::write.config(list(
-    defaults = list(
+  report.name <- readline(prompt = "Name of report to download: ")
+  config <- list(
+    global = list(
       api_user = readline(prompt = "API User name: "),
       api_pass = getPass::getPass(msg = "API User Password: ")
-    ),
-    reports = list(
-      title = readline(prompt = "Name of report to download: "),
-      url = getPass::getPass(msg = "URL of report to download (with api_key): ")
     )
-  ), tmpfile, write.type = "ini")
+  )
+  config[[report.name]] <- list(
+    url = getPass::getPass(msg = "URL of report to download (with api_key): ")
+  )
+  configr::write.config(
+    config, tmpfile, write.type = "ini")
   writeLines(gpg::gpg_encrypt(data = tmpfile, receiver = receiver), path.for.secrets)
   on.exit(unlink(tmpfile))
+  if (return.creds) {
+    return(config)
+  }
+  return()
 }
--- R/reports.R
+++ R/reports.R
@@ -4,8 +4,37 @@
 #
 #
 get.report <- function(credentials, report.name) {
-  return(httr::GET(
-    credentials$reports$url,
-    httr::authenticate(credentials$defaults$api_user, credentials$defaults$api_pass)
-  ))
+  # Get the report as xml from LegalServer using supplied credentials
+  message("Downloading report...")
+  xml.response <- httr::GET(
+    credentials[[report.name]]$url,
+    httr::authenticate(credentials$global$api_user, credentials$global$api_pass)
+  )
+
+  message("Parsing xml...")
+  # Parse the xml as an xml doc, using xml2.
+  doc <- httr::content(xml.response, as="parsed", type="text/xml", encoding="utf-8")
+
+  # Extract the column names.
+  # This assumes that every row in the report xml has the same columns as the first row.
+  # Because the xml is an xml-encoding of a 2-d table, I think this is a fine assumption to
+  # make.
+  columns <- xml_name(
+              xml_children(
+                xml_find_first(doc, "row")))
+
+  message("Initializing empty dataframe of the length of the report.")
+  df <- data.frame(row.names = seq(1, length(xml2::xml_find_all(doc, "//row"))))
+  # For each column, add the values of that column to a dataframe.
+  message("Converting to a friendly dataframe...")
+  mapply(
+    function(col.name, df, doc) {
+      message("Processing column: ", col.name)
+      df[col.name] <- xml2::xml_find_all(doc, paste0("//",col.name,"/text()"))
+    },
+    columns,
+    MoreArgs = list(df,doc)
+  )
+  message("All done!")
+  return(df)
 }
