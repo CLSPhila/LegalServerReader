695247bdb4028dce46f515c87669da86c40d1317
Nate Vogel <nathan.c.vogel@gmail.com>
2019-02-05 21:10:30 -0500

    refactored credentials so that its a more pipeliny process, and it can handle multiple reports in a single config file

--- R/credentials.R
+++ R/credentials.R
@@ -13,29 +13,64 @@ get.credentials <- function(credentials.path = "./credentials.gpg") {
     configr::read.config(file = tmpfile))
 }
 
-# Save credentials
-#
-# Ask a user for credentials to download reports from LegalServer, and
-# save those credentials to a file encrypted with gpg.
-save.credentials <- function(receiver, path.for.secrets = "./credentials.gpg", return.creds = FALSE) {
+
+#' Create Credentials
+#'
+#' Create a credentials list with just the credentials for the api user.
+#' No reports stored here yet.
+create.credentials <- function() {
   writeLines("Please enter the legalserver api credentials");
-  tmpfile <- tempfile()
-  report.name <- readline(prompt = "Name of report to download: ")
-  config <- list(
-    global = list(
-      api_user = readline(prompt = "API User name: "),
-      api_pass = getPass::getPass(msg = "API User Password: ")
+  return(
+    list(
+      global = list(
+        api_user = readline(prompt = "API User name: "),
+        api_pass = getPass::getPass(msg = "API User Password: ")
+      )
     )
   )
-  config[[report.name]] <- list(
+}
+
+#' Save Report Key
+#'
+#' Add a report and report url to a credentials list.
+#'
+add.report <- function(credentials, report.name) {
+  credentials[[report.name]] <- list(
     url = getPass::getPass(msg = "URL of report to download (with api_key): ")
   )
+  return(credentials)
+}
+
+#' Remove a report from a credentials list
+remove.report <- function(credentials, report.name) {
+  credentials[report.name] <- NULL
+  return(credentials)
+}
+
+
+#' List reports available in a set of credentials
+list.reports <- function(credentials) {
+  return(
+    names(
+      credentials[names(creds) != "global"]
+    )
+  )
+}
+
+# Save credentials
+#
+# Save credentials to a file encrypted with gpg.
+save.credentials <- function(credentials,
+                             receiver,
+                             path.for.secrets = "./credentials.gpg",
+                             return.creds = FALSE) {
+  tmpfile <- tempfile()
   configr::write.config(
-    config, tmpfile, write.type = "ini")
+    credentials, tmpfile, write.type = "ini")
   writeLines(gpg::gpg_encrypt(data = tmpfile, receiver = receiver), path.for.secrets)
   on.exit(unlink(tmpfile))
   if (return.creds) {
-    return(config)
+    return(credentials)
   }
   return()
 }
